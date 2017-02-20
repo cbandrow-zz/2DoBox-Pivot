dc1378ef1c6b7e822022711bc64c08a552c54986
Travis <tlgreg86@gmail.com>
2017-02-20 14:22:56 -0700

    Add test 5 and 6. Test upvote button

---
+++ selenium-tests/test6.js
@@ -0,0 +1,43 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+
+var driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+var driver_fx = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+// var driver_saf = new webdriver.Builder()
+//     .forBrowser('safari')
+//     .build();
+
+searchTest(driver_chr);
+searchTest(driver_fx);
+// searchTest(driver_saf);
+
+
+function searchTest(driver) {
+  driver.get('https://cbandrow.github.io/2DoBox-Pivot/');
+  driver.findElement(By.id('title-input')).sendKeys('TEST!!');
+  driver.findElement(By.id('body-input')).sendKeys('THIS IS A TEST OF THE EMERGENCY BROADCAST SYSTEM: THIS IS ONLY A TEST. PLEASE REMAIN CALM.');
+  driver.findElement(By.id('save-button')).click();
+  driver.navigate().refresh();
+  driver.findElement(By.className('upvote-button')).click();
+  driver.findElement(By.className('upvote-button')).click();
+
+  driver.sleep(3000).then(function() {
+    driver.findElement(By.className('current-quality')).getText().then(function(title) {
+      if(title === 'genius') {
+        console.log('Test passed');
+      } else {
+        console.log('Test failed');
+      }
+    });
+  });
+
+  driver.quit();
+  }
