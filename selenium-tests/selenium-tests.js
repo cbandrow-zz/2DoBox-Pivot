29d26728328ec762568571b40c24679684decfa9
Travis <tlgreg86@gmail.com>
2017-02-21 11:45:18 -0700

    Update test 11

---
+++ selenium-tests/selenium-tests.js
@@ -0,0 +1,1 @@
+// I've spoken with a few people and they are running all of their tests at once in one js file.
--- selenium-tests/test11.js
+++ selenium-tests/test11.js
@@ -25,26 +25,22 @@ function searchTest(driver) {
   driver.findElement(By.id('title-input')).sendKeys('TEST!!');
   driver.findElement(By.id('body-input')).sendKeys('THIS IS A TEST OF THE EMERGENCY BROADCAST SYSTEM: THIS IS ONLY A TEST. PLEASE REMAIN CALM.');
   driver.findElement(By.id('save-button')).click();
-  driver.navigate().refresh();
-  driver.findElement(By.className('idea-title')).click();
-  driver.findElement(By.className('idea-title')).clear();
-  driver.findElement(By.className('idea-title')).sendKeys(Key.DELETE);
-  // var element = driver.findElement(By.className('idea-title'));
-  // element.doubleClick();
-  // element.sendKeys("new test");
-  // driver.findElement(By.className('idea-title')).sendKeys(Key.META, "a", Key.NULL, "new test");
-  driver.findElement(By.className('make-idea-teal')).click();
+  driver.findElement(By.className('idea-title')).sendKeys('HELLO!');
+  driver.findElement(By.className('top-section')).click();
 
+  driver.navigate().refresh();
 
   driver.sleep(3000).then(function() {
     driver.findElement(By.className('idea-title')).getText().then(function(title) {
-      if(title === 'new test') {
+      if(title != 'TEST!!') {
         console.log('Test passed');
       } else {
         console.log('Test failed');
       }
     });
   });
+
+
 
   driver.quit();
   }
